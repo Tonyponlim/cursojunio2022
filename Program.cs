49a064729798b45ba759df09cf5cda7ff56217f3
Tonyponlim <85765933+Tonyponlim@users.noreply.github.com>
2022-06-17 19:01:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsolaAplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!");
+            Console.ReadKey();
+            int num1 = 5;
+            int nuero2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El número introducido es: " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+            Console.ReadKey();
+        }
+    }
+}
